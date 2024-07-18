b4bed94a2b3cc447963aa2c283efc26992df933e
tuitenthai <21004148@st.vlute.edu.vn>
2024-07-18 21:44:53 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="p-2">
+      <div className="w-full h-[200px] p-2 bg-cyan-700 rounded-lg flex items-center justify-center">
+        <span className="text-white">conmeo</span>
+      </div>
+    </div>
+  )
+}
